3dc4fe6aec1c53c89a284002f598216420b480bb
git99.src <cpu.al2@gmail.com>
2020-11-11 21:59:27 -0800

    Created code template

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  "imagebaseurl":"http://localhost:3000/",
+  "name": "Al Pineda",
+  "role": "Full Stack Developer",
+  "linkedinId":"al-pineda-13194955",
+  "skypeid": "Your skypeid",
+  "roleDescription": " With the fast paced evolution of technology and software, I continually seek ways to accelerate my growth within the industry. It is my belief that building and designing solutions starts with listening.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/al-pineda-13194955",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/git99-src",
+        "className":"fa fa-github"
+      },
+      // {
+      //   "name":"skype",
+      //   "url":"http://skype.com",
+      //   "className":"fa fa-skype"
+      // }
+    ],
+  "aboutme":"I have been a software developer with 27 years of experience in the Healthcare industry.  My varied background includes programming, software design, research and development and IT management",
+  // "address":"India",
+  // "website":"https://www.codeclassifiers.com",
+  "education":[
+    {
+      "UniversityName":"Mumbai University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Some University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
